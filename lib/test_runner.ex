1487505c15a4a8be53653b692900fad50b6d05a6
Bruce <brucepark0@gmail.com>
2019-01-29 23:47:20 -0800

    Add message

--- lib/test_runner.ex
+++ lib/test_runner.ex
@@ -3,7 +3,7 @@ defmodule Padawan.TestRunner do
     Enum.each(tests, fn {line_num, test_function, description} ->
       case apply(module, test_function, []) do
         {:ok, _msg} ->
-          IO.write("Line: #{line_num} passing...")
+          IO.write("Line #{line_num}, #{description} passing...")
       end
     end)
   end
